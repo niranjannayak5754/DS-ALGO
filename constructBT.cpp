24db7e53f7db37dd7e86cff2fcb9a7b1152f2096
niranjan <niranjan.nayak@ripplr.in>
2023-12-02 17:18:27 +0530

    construct binary tree using preorder and inorder traversal

---
+++ constructBT.cpp
@@ -0,0 +1,54 @@
+/*
+    Construct a binary tree from two arrays inorder and preorder traversal of tree. [Medium]
+
+    inorder traversal -> left root right
+    preorder traversal -> root left right
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//  Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+private:
+    TreeNode *buildTree(vector<int> &preorder, int preStart, int preEnd, vector<int> &inorder, int inStart, int inEnd, unordered_map<int, int> &inMap)
+    {
+        if (preStart > preEnd || inStart > inEnd)
+            return nullptr;
+
+        TreeNode *root = new TreeNode(preorder[preStart]);
+
+        int inRoot = inMap[root->val];
+        int numsLeft = inRoot - inStart;
+
+        root->left = buildTree(preorder, preStart + 1, preStart + numsLeft, inorder, inStart, inRoot - 1, inMap);
+        root->right = buildTree(preorder, preStart + numsLeft + 1, preEnd, inorder, inRoot + 1, inEnd, inMap);
+
+        return root;
+    }
+
+public:
+    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder)
+    {
+        unordered_map<int, int> inMap;
+
+        for (int i = 0; i < inorder.size(); i++)
+        {
+            inMap[inorder[i]] = i;
+        }
+
+        return buildTree(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1, inMap);
+    }
+};
